e033fbaed3fb36c2ea06fe879505469113373448
Gabrielzbr7419 <gschmidt7410@gmail.com>
2025-09-20 12:52:47 -0300

    jogo da memoria

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,24 @@ function urlBuilder(number){
 }
 let card = new CardManager(urlBuilder);
 let board = new BoardManager("board", 5, card);
+
+let menu = document.getElementById('menu');
+let select = document.getElementById('numCards');
+let start = document.getElementById('start');
+
+for (let i =4; i <= 10; i+2) {
+    let n = i*i;
+    let op = document.createElement('option');
+
+    op.value=n;
+    op.innerHTML=n;
+
+    select.appendChild(op);
+}
+
+
+start.addEventListener("click", ()=> { 
+    menu.classList.add("hidden");
+    board.node.classlist.remove("hidden");
+    board.fill(select.value);
+});
