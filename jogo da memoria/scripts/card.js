6b8c11703c273682603f56e69533b8825f75c754
Gabrielzbr7419 <gschmidt7410@gmail.com>
2025-09-27 12:44:16 -0300

    Add files via upload

---
+++ jogo da memoria/scripts/card.js
@@ -0,0 +1,24 @@
+class CardManager {
+
+flippedCards = new Set();
+urlFactory;
+
+constructor(factory) {
+    this.urlFactory = factory
+}
+
+gen(heroNumber) {
+    let template = document.getElementById("cardTemplate");
+    let clone = template.content.cloneNode(true);
+
+    let img = clone.querySelector("img");
+
+    img.setAttribute('src', this.urlFactory(heroNumber));
+
+    clone.children[0].addEventListener('click',
+        event => this.onClick(event)
+    );
+
+    return clone;       
+}
+}
---
+++ jogo da memoria/scripts/main.js
@@ -0,0 +1,38 @@
+function urlBuilder(number){
+    number+="";
+    number =number.padStart(2,'0');
+
+    return `images/heros/hero${number}.webp`;
+}
+let card = new CardManager(urlBuilder);
+let board = new BoardManager("board", 5, card);
+
+let menu = document.getElementById('menu');
+let select = document.getElementById('numCards');
+let start = document.getElementById('start');
+
+for (let i =4; i <= 10; i+2) {
+    let n = i*i;
+    let op = document.createElement('option');
+
+    op.value=n;
+    op.innerHTML=n;
+
+    select.appendChild(op);
+}
+
+
+start.addEventListener("click", ()=> { 
+    menu.classList.add("hidden");
+    board.node.classlist.remove("hidden");
+    board.fill(select.value);
+});
+
+board.note.addEventListener("click", ()=> {
+    if (board.check()){
+        setTimeout(() => {
+            menu.classList.remove("hidden");
+            board.node.classList.add("hidden");
+        }, 2000);
+    }
+});
